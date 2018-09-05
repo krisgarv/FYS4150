e2480d698d21551e8431515653509bd12a5c051f
Silje Christine Iversen <siljeci@student.matnat.uio.no>
2018-09-05 22:53:55 +0200

    FUNKER FORTSATT IKKEEEEE

--- Problemsets/1st/oppg1b.py
+++ Problemsets/1st/oppg1b.py
@@ -8,7 +8,7 @@ n = int(sys.argv[1])
 
 #Matrix values:
 a = -1.0
-b= 2.0
+b = 2.0
 c = -1.0
 
 #Defining x, and the step size h:
@@ -40,18 +40,18 @@ d_hat		= np.zeros(n)
 for i in range(0, n):
 	d_vec[i] = f(x[i+1])*h**2
 
-#Inserting values for b_tilde, d_tilde and d_hat:
+#Inserting known values for b_tilde and d_tilde:
 b_tilde[0] = b_vec[0]
 
 b_tilde[1] = b_vec[1] - c_vec[0]*a_vec[0]/b_tilde[0]
 
 d_tilde[0] = d_vec[0]
 
-d_tilde[1] = d_vec[1] - d_tilde[0]*a_vec[0]/b_vec[0]
+d_tilde[1] = d_vec[1] - d_tilde[0]*a_vec[0]/b_tilde[0]
 
 #Looping to find b_tilde, d_tilde and d_hat:
 t0 = time.time()
-for i in range(2, n):
+for i in range(2, n):	#b3 -> bn
 	b_tilde[i] = b_vec[i] - c_vec[i-1]*a_vec[i-1]/b_tilde[i-1]
 	d_tilde[i] = d_vec[i] - d_tilde[i-1]*a_vec[i-1]/b_tilde[i-1]
 
@@ -61,7 +61,23 @@ for i in range(n-2, -1, -1):
 	d_hat[i] = d_tilde[i] - d_tilde[i+1]*c_vec[i]/b_tilde[i+1]
 t1 = time.time()
 
-#b_tilde * v = d_hat
+#Numerical answer: 
+for i in range(1, n+1):	#we want to find v_1 and up til v_n, and we have to use d_hat_1 and up til d_hat_n
+	v[i] = d_hat[i-1]/float(b_tilde[i-1])
+
+#Analytic answer: 
+def u(x): 
+	return 1 - (1 - np.exp(-10))*x - np.exp(-10*x)
+
+#TEST!!
+print(np.max(u(x) - v))
+
+
+#Plot:
+plt.plot(x, v, x, u(x))
+plt.legend( ["data", "analytic"] )
+plt.title("For n=%d" % (n))
+plt.show()
 
 print("Time used: ",(t1-t0)," s")
 
