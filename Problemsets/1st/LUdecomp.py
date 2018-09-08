7c908bbe06c3869bb4c238bb4700a402b3377c08
Kristine Garvin <krisgarv@student.geo.uio.no>
2018-09-08 20:07:57 +0200

    ferdig, med kommentarer

--- Problemsets/1st/LUdecomp.py
+++ Problemsets/1st/LUdecomp.py
@@ -1,3 +1,4 @@
+# LU-decomposition
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
@@ -5,37 +6,38 @@ import time
 import scipy
 import scipy.linalg as sl
 
-# LU-decomposition
-
-#Getting the matrix-size from the terminal:
+# Getting the matrix-size from the terminal:
 n = int(sys.argv[1])
-#n=10
 
+# Array of zeros of length [0, n+1]:
 x = np.linspace(0, 1, n+2)
 v = np.array(np.zeros_like(x))
+# Array of length [1, n]:
 d = np.zeros(n)
+# Defining the step size:
 h = 1.0/(n+1)
 
-
+# Defining a function which calculates f(x):
 def f(x):
 	return 100*np.exp(-10*x)
 
+# Defining a function which calculates our analytical solution:
 def u(x):
 	return 1 - (1 - np.exp(-10))*x - np.exp(-10*x)
 
-#Looping to find d-vector:
+# Defining the vector d (RHS):
 for i in range(0, n):
 	d[i] = f(x[i+1])*h**2
 
-# Defining the matrix A as a nested list as this does not need predefined size:
+# Defining the matrix A as an empty nested list:
 M = []
-# Defining first row vector of size n:
+# Defining first row in matrix:
 r_0 = np.zeros(n)
 r_0[0] = 2.0
 r_0[1] = -1.0
 # Appending first row in matrix
 M.append(r_0)
-# Looping over middle rows in matrix, appending initial values a=-1, b=2, c=-1,
+# Looping over row [2, n-1] in matrix, appending initial values a=-1, b=2, c=-1,
 # along the diagonal and non-diagonal:
 for i in range(0, n-2):
     r = np.zeros(n)
@@ -55,24 +57,41 @@ A = np.array(M)
 
 # The LU-decomposition is done in two steps
 # with functions from the scipy.linalg library
-# lu (matrix) - Calculate LU matrix containing U in its upper triangle, and L in its lower
-# triangle. The unit diagonal elements of L are not stored.
+# First we use lu_factor which gives a factorization of our matrix A.
+# lu (matrix) - Calculate LU matrix containing U in its upper triangle,
+# and L in its lower triangle. The unit diagonal elements of L are not stored.
 # piv (array) - Pivot indices representing the permutation matrix P:
 # row i of matrix was interchanged with row piv[i].
+# To increase performance:
+# overwrite_a = True, lets the function reuse the input matrix
+# check_finite = False, the function will not check that the input matrix
+# contains only finite numbers.
 lu, piv = sl.lu_factor(A, overwrite_a=True, check_finite=False)
-#
+# The lu_solve function solves the equation system Av = d,
+# given the LU factorization from lu_factor.
+# w (array) - is the solution of the system, does not include the
+# boundary conditions.
+# To increase performance:
+# overwrite_b=True, allows the function to reuse the input vector.
+# check_finite=False, the function will not check that the input matrix
+# contains only finite numbers.
+# The time of this operation is logged:
 t0 = time.time()
 w = sl.lu_solve((lu, piv), d, trans=0, overwrite_b=True, check_finite=False)
 t1 = time.time()
-
-timer=t1-t0
-with open('logfile.txt', 'a') as log:
-	log.write('LU - n=%d, Timer: %.17f s\n' % (n, timer))
-
+# Appending the results to final solution where v[0] = v[n+1] = 0
 v[1:-1] = w
-#print(v)
+
+# Calculating time spendt by the LU solver:
+timer=t1-t0
+"""
+# Plotting Numerical solution compared to analytical
 plt.plot(x, v, x, u(x))
 plt.legend( ["Numerical", "Analytic"] )
 plt.title("n=%d" % (n))
 #plt.show()
 print("Time used:" , timer, "s")
+"""
+# Writing the time spendt to a logfile for further evaluation:
+with open('logfile.txt', 'a') as log:
+	log.write('LU - n=%d, CPU time: %.17f s\n' % (n, timer))
