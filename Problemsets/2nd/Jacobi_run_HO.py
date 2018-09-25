eaf302a23a054b68a9c3d82df96a2ac291b8737f
Kristine Garvin <krisgarv@student.geo.uio.no>
2018-09-25 07:32:24 +0200

    Ja, hva med rho_max..?

--- Problemsets/2nd/Jacobi_run_HO.py
+++ Problemsets/2nd/Jacobi_run_HO.py
@@ -13,33 +13,40 @@ a = (1.0/h**2)*-1.0
 di = np.zeros(N+1)
 for i in range(0,N):
     di[i] = (2.0 + (i*h)**2)/h**2
-# Creating the input matrix
-r = np.zeros(N)
-r[0] = 0
-r[1] = a
-A = sl.toeplitz(r, r)
-np.fill_diagonal(A, di)
 
-# Calling the Jacobi module with initial values for buckling beam.
-i = J(A)
+# ANALYTIC RESULTS FOR THE EIGENVALUES L = [3, 7, 11, 15, ...]
+# HVORDAN FINNE EN RHO SOM MATCHER?
 
-# Numpys solution:
-t0 = time.time()
-numpy_lmbda = i.nmpy_eigenval()
-t1 = time.time()
-time_numpy = t1 - t0
+def run(N, a, di):
+    # Creating the input matrix
+    r = np.zeros(N)
+    r[0] = 0
+    r[1] = a
+    A = sl.toeplitz(r, r)
+    np.fill_diagonal(A, di)
 
-# Sorting eigenvalues by size to simplify comparison.
-NA = np.sort(numpy_lmbda)
+    # Calling the Jacobi module with initial values for buckling beam.
+    i = J(A)
 
-# Jacobi solution:
-t2 = time.time()
-Jacobi_A, Jacobi_R, Jacobi_iter = i.Jacobi()
-t3 = time.time()
-time_jacobi = t3 - t2
+    # Numpys solution:
+    t0 = time.time()
+    numpy_lmbda = i.nmpy_eigenval()
+    t1 = time.time()
+    time_numpy = t1 - t0
 
-# Sorting eigenvalues by size to simplify comparison.
-JA = np.sort(np.diag(Jacobi_A))
+    # Sorting eigenvalues by size to simplify comparison.
+    NA = np.sort(numpy_lmbda)
+
+    # Jacobi solution:
+    t2 = time.time()
+    Jacobi_A, Jacobi_R, Jacobi_iter = i.Jacobi()
+    t3 = time.time()
+    time_jacobi = t3 - t2
+
+    # Sorting eigenvalues by size to simplify comparison.
+    JA = np.sort(np.diag(Jacobi_A))
+
+    return Jacobi_A, numpy_lmbda time_jacobi, time_numpy
 
 
 # Print to commandline:
