51e49cb3586a51a61844edd3cb243836a3b36278
Silje Christine Iversen <siljeci@student.matnat.uio.no>
2018-10-18 15:44:49 +0200

    To use when the Sun is in the senter, only plots

--- Problemsets/3rd/SolarSystem_plot.py
+++ Problemsets/3rd/SolarSystem_plot.py
@@ -5,8 +5,28 @@ from mpl_toolkits.mplot3d import Axes3D
 import sys
 
 """
-To use: Write into terminal:
+------------------------------------------------
+USE WHEN ORIGO IS IN THE CENTER OF THE SUN
+------------------------------------------------
 
+Plotting the 2-body or 3-body system in 2D or 3D.
+For 2-body in 2D:
+Using the initial velocity for the Earth found as v = 2pi.
+
+For 2-body in 3D:
+Using the initial velocity for the Earth found from NASA HORIZON
+(to be able to have a initial w (z-direction)).
+
+For 3-body in 2D:
+Using the initial velocity for the Earth and Jupiter found from
+NASA HORIZON.
+
+For 3-body in 3D:
+Using the inital velocity for the Earth and Jupiter found from
+NASA HORIZON.
+------------------------------------------------
+
+To use, write into terminal:
 python SolarSystem_plot.py arg1 arg2 arg3 arg4 arg 5 arg6 ...
 
 arg1: Dimensions to be plotted. Enter 2 for 2D or 3 for 3D
@@ -41,12 +61,12 @@ def input_matrix_2D(numbodies):
     else:
         #Init.positions:
         position_sun = (0.0, 0.0, 0.0)
-        position_earth = (1.003549362691021, 1.410236892872957E-03, -7.890400039667053E-05)
-        position_jupiter = (-2.795075614391798, -4.586076524625780, 8.154235484924463E-02)
+        position_earth = (1.003538121253694, -5.782251806221399E-03, -2.257327676060083E-06)
+        position_jupiter = (-2.795086855829125, -4.593269013324874, 8.161900152196526E-02)
         #Init.velocities:
         velocity_sun = (0.0, 0.0, 0.0)
-        velocity_earth = (365*-1.909119143085214E-04, 365*1.713603683351200E-02, 365*-3.683319869509187E-08)
-        velocity_jupiter = (365*6.353601024346235E-03, 365*-3.567676179080474E-03, 365*-1.273208346688556E-04)
+        velocity_earth = (365*-1.833830875310781E-04, 365*1.713325008634928E-02, 365*-2.247700714731761E-07)
+        velocity_jupiter = (365*6.361129851123679E-03, 365*-3.570462926243191E-03, 365*-1.275087715416329E-04)
         #Making initial matrix:
         input_matrix[0, 0] = mass_sun
         input_matrix[1, 0] = mass_earth
@@ -64,10 +84,10 @@ def input_matrix_3D(numbodies):
     if numbodies == 2:
         #Init.positions:
         position_sun = (0.0, 0.0, 0.0)
-        position_earth = (1.003549362691021, 1.410236892872957E-03, -7.890400039667053E-05)
+        position_earth = (1.003538121253694, -5.782251806221399E-03, -2.257327676060083E-06)
         #Init.velocities:
         velocity_sun = (0.0, 0.0, 0.0)
-        velocity_earth = (365*-1.909119143085214E-04, 365*1.713603683351200E-02, 365*-3.683319869509187E-08)
+        velocity_earth = (365*-1.833830875310781E-04, 365*1.713325008634928E-02, 365*-2.247700714731761E-07)
         #Making initial matrix:
         input_matrix[0, 0] = mass_sun
         input_matrix[1, 0] = mass_earth
@@ -78,12 +98,12 @@ def input_matrix_3D(numbodies):
     else:
         #Init.positions:
         position_sun = (0.0, 0.0, 0.0)
-        position_earth = (1.003549362691021, 1.410236892872957E-03, -7.890400039667053E-05)
-        position_jupiter = (-2.795075614391798, -4.586076524625780, 8.154235484924463E-02)
+        position_earth = (1.003538121253694, -5.782251806221399E-03, -2.257327676060083E-06)
+        position_jupiter = (-2.795086855829125, -4.593269013324874, 8.161900152196526E-02)
         #Init.velocities:
         velocity_sun = (0.0, 0.0, 0.0)
-        velocity_earth = (365*-1.909119143085214E-04, 365*1.713603683351200E-02, 365*-3.683319869509187E-08)
-        velocity_jupiter = (365*6.353601024346235E-03, 365*-3.567676179080474E-03, 365*-1.273208346688556E-04)
+        velocity_earth = (365*-1.833830875310781E-04, 365*1.713325008634928E-02, 365*-2.247700714731761E-07)
+        velocity_jupiter = (365*6.361129851123679E-03, 365*-3.570462926243191E-03, 365*-1.275087715416329E-04)
         #Making initial matrix:
         input_matrix[0, 0] = mass_sun
         input_matrix[1, 0] = mass_earth
@@ -106,19 +126,27 @@ def plot_2D(input_matrix, numbodies, method, time_max, numsteps):
             euler = solver(input_matrix, 'euler', time_max, num)
             out_euler, KE_euler, PE_euler, AM_euler = euler.main()
             if numbodies == 2:
+                xEs = out_euler[0, 0, :]
+                yEs = out_euler[0, 1, :]
                 xEe = out_euler[1, 0, :]
                 yEe = out_euler[1, 1, :]
                 plt.figure(i, figsize=(10, 10))
-                plt.plot(xEe, yEe, 'y')
+                plt.plot(xEs, yEs, 'y')
+                plt.plot(xEe, yEe, 'g')
+                legend.append("Sun")
                 legend.append("Earth")
             else:
+                xEs = out_euler[0, 0, :]
+                yEs = out_euler[0, 1, :]
                 xEe = out_euler[1, 0, :]
                 yEe = out_euler[1, 1, :]
                 xEj = out_euler[2, 0, :]
                 yEj = out_euler[2, 1, :]
                 plt.figure(i, figsize=(10, 10))
-                plt.plot(xEe, yEe, 'y')
+                plt.plot(xEs, yEs, 'y')
+                plt.plot(xEe, yEe, 'g')
                 plt.plot(xEj, yEj, 'r')
+                legend.append("Sun")
                 legend.append("Earth")
                 legend.append("Jupiter")
 
@@ -126,25 +154,33 @@ def plot_2D(input_matrix, numbodies, method, time_max, numsteps):
             verlet = solver(input_matrix, 'verlet', time_max, num)
             out_verlet, KE_verlet, PE_verlet, AM_verlet = verlet.main()
             if numbodies == 2:
+                xVs = out_verlet[0, 0, :]
+                yVs = out_verlet[0, 1, :]
                 xVe = out_verlet[1, 0, :]
                 yVe = out_verlet[1, 1, :]
                 plt.figure(i, figsize=(10, 10))
-                plt.plot(xVe, yVe, 'y')
+                plt.plot(xVs, yVs, 'y')
+                plt.plot(xVe, yVe, 'g')
+                legend.append("Sun")
                 legend.append("Earth")
             else:
+                xVs = out_verlet[0, 0, :]
+                yVs = out_verlet[0, 1, :]
                 xVe = out_verlet[1, 0, :]
                 yVe = out_verlet[1, 1, :]
                 xVj = out_verlet[2, 0, :]
                 yVj = out_verlet[2, 1, :]
                 plt.figure(i, figsize=(10, 10))
-                plt.plot(xVe, yVe, 'y')
+                plt.plot(xVs, yVs, 'y')
+                plt.plot(xVe, yVe, 'g')
                 plt.plot(xVj, yVj, 'r')
+                legend.append("Sun")
                 legend.append("Earth")
                 legend.append("Jupiter")
         plt.figure(i)
         plt.xlabel('x [AU]')
         plt.ylabel('y [AU]')
-        plt.title('Orbit around the Sun (in origo) by using %s, N=%i' %(method,num))
+        plt.title('Orbits (Sun in origo) by using %s, N=%i' %(method,num))
         plt.legend(legend, loc=2, fontsize='small')
         plt.axis('equal')
         plt.show()
@@ -159,14 +195,22 @@ def plot_3D(input_matrix, numbodies, method, time_max, numsteps):
             euler = solver(input_matrix, 'euler', time_max, num)
             out_euler, KE_euler, PE_euler, AM_euler = euler.main()
             if numbodies == 2:
+                xEs = out_euler[0, 0, :]
+                yEs = out_euler[0, 1, :]
+                zEs = out_euler[0, 2, :]
                 xEe = out_euler[1, 0, :]
                 yEe = out_euler[1, 1, :]
                 zEe = out_euler[1, 2, :]
                 fig = plt.figure(i, figsize=(10,10))
                 ax = fig.gca(projection='3d')
-                ax.plot(xEe, yEe, zEe, 'y')
+                ax.plot(xEs, yEs, zEs, 'y')
+                ax.plot(xEe, yEe, zEe, 'g')
+                legend.append("Sun")
                 legend.append("Earth")
             else:
+                xEs = out_euler[0, 0, :]
+                yEs = out_euler[0, 1, :]
+                zEs = out_euler[0, 2, :]
                 xEe = out_euler[1, 0, :]
                 yEe = out_euler[1, 1, :]
                 zEe = out_euler[1, 2, :]
@@ -175,8 +219,10 @@ def plot_3D(input_matrix, numbodies, method, time_max, numsteps):
                 zEj = out_euler[2, 2, :]
                 fig = plt.figure(i, figsize=(10, 10))
                 ax = fig.gca(projection='3d')
-                ax.plot(xEe, yEe, zEe, 'y')
+                ax.plot(xEs, yEs, zEs, 'y')
+                ax.plot(xEe, yEe, zEe, 'g')
                 ax.plot(xEj, yEj, zEj, 'r')
+                legend.append("Sun")
                 legend.append("Earth")
                 legend.append("Jupiter")
 
@@ -184,14 +230,22 @@ def plot_3D(input_matrix, numbodies, method, time_max, numsteps):
             verlet = solver(input_matrix, 'verlet', time_max, num)
             out_verlet, KE_verlet, PE_verlet, AM_verlet = verlet.main()
             if numbodies == 2:
+                xVs = out_verlet[0, 0, :]
+                yVs = out_verlet[0, 1, :]
+                zVs = out_verlet[0, 2, :]
                 xVe = out_verlet[1, 0, :]
                 yVe = out_verlet[1, 1, :]
                 zVe = out_verlet[1, 2, :]
                 fig = plt.figure(i, figsize=(10,10))
                 ax = fig.gca(projection='3d')
-                ax.plot(xVe, yVe, zVe, 'y')
+                ax.plot(xVs, yVs, zVs, 'y')
+                ax.plot(xVe, yVe, zVe, 'g')
+                legend.append("Sun")
                 legend.append("Earth")
             else:
+                xVs = out_verlet[0, 0, :]
+                yVs = out_verlet[0, 1, :]
+                zVs = out_verlet[0, 2, :]
                 xVe = out_verlet[1, 0, :]
                 yVe = out_verlet[1, 1, :]
                 zVe = out_verlet[1, 2, :]
@@ -200,17 +254,20 @@ def plot_3D(input_matrix, numbodies, method, time_max, numsteps):
                 zVj = out_verlet[2, 2, :]
                 fig = plt.figure(i, figsize=(10,10))
                 ax = fig.gca(projection='3d')
-                ax.plot(xVe, yVe, zVe, 'y')
+                ax.plot(xVs, yVs, zVs, 'y')
+                ax.plot(xVe, yVe, zVe, 'g')
                 ax.plot(xVj, yVj, zVj, 'r')
+                legend.append("Sun")
                 legend.append("Earth")
                 legend.append("Jupiter")
         ax.set_xlabel('x [AU]')
         ax.set_ylabel('y [AU]')
         ax.set_zlabel('z [AU]')
-        ax.set_title('Orbit around the Sun (in origo) by using %s, N=%i' %(method,num))
+        ax.set_title('Orbits (Sun in origo) by using %s, N=%i' %(method,num))
         ax.legend(legend, loc=2, fontsize='small')
         plt.axis('equal')
         plt.show()
+
 #----------------------------------------------------------------------------
 
 def start():
@@ -229,62 +286,5 @@ def start():
 
 #------------------------------------------------------------------------------
 
-"""
-def energies():
-    numsteps = 10000
-    time_max = 1
-    verlet  = solver(input_matrixelse:
-            plt.figure(1)
-            plt.plot(xEe, yEe)
-            plt.figure(2)
-            plt.plot(xVe, yVe)
-
-            legend.append(str(num))
-        for i in range(1, 3):
-            plt.figure(i)
-            plt.xlabel('x [AU]\n %i year(s)' %(time_max))
-            plt.ylabel('y [AU]')
-            plt.title("Earth's orbit around the Sun (in origo)")
-            plt.legend(legend, loc=2, fontsize='small')
-            plt.axis('equal'), 'verlet', time_max, numsteps)
-    output_matrix, KE, PE, AM = verlet.main()
-    x = np.linspace(0, 1, numsteps+1)
-
-    plt.figure(1, figsize=(10, 10))
-    plt.plot(x, KE)
-    plt.suptitle('Total kinetic energy in the Earth-Sun system.', fontsize=24)
-    plt.xlabel('time [yr]', fontsize=16)
-    plt.ylabel('energy [AU²*kg/yr²]', fontsize=16)
-    plt.legend(['KE'])
-
-    plt.figure(2, figsize=(10, 10))
-    plt.plot(x, PE)
-    plt.suptitle('Total potential energy in the Earth-Sun system.', fontsize=24)
-    plt.xlabel('time [yr]', fontsize=16)
-    plt.ylabel('energy [AU²*kg/yr²]', fontsize=16)
-    plt.legend(['PE'])
-
-    plt.figure(3, figsize=(10, 10))
-    plt.plot(x, PE+KE)
-    plt.suptitle('Total energy in the Earth-Sun system.', fontsize=24)
-    plt.xlabel('time [yr]', fontsize=16)
-    plt.ylabel('energy [AU²*kg/yr²]', fontsize=16)
-    plt.legend(['KE+PE'])
-
-    plt.figure(4, figsize=(10, 10))
-    plt.plot(x, AM)
-    plt.suptitle('Total angular momentum in the Earth-Sun system.', fontsize=24)
-    plt.xlabel('time [yr]', fontsize=16)
-    plt.ylabel('energy [AU²/yr²]', fontsize=16)
-    plt.legend(['AM'])
-
-    plt.figure(5, figsize=(10, 10))
-    plt.plot(x, PE, x, KE, x, KE+PE)
-    plt.suptitle('Total energy in the Earth-Sun system.', fontsize=24)
-    plt.xlabel('time [yr]', fontsize=16)
-    plt.ylabel('energy [AU²*kg/yr²]', fontsize=16)
-    plt.legend(['PE', 'KE', 'KE+PE'])
-    plt.show()
-"""
 if __name__=="__main__":
     start()
