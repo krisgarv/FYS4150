3f60f217158f8b4c5a8be77fa93d18391f395bd7
krisgarv <kristinegarvin@gmail.com>
2018-12-07 18:35:16 +0100

    Vet ikke om det ble noe bedre?

--- Problemsets/5th/run.py
+++ Problemsets/5th/run.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import solver as s
 #===========Initial variables for CrankNicolson===============================
-N = 10
-T = 200
+N = 100
+T = 50000
 alpha = 0.5
 # Initial conditions v(0, x) = u(0, x) - x = -x
 x = np.linspace(0, 1, N+1)
 # Initial matrix for storing v-values
 # v[t, x]
-vi = np.zeros((T+1, N+1))
-for t in range(T+1):
-    vi[t, :] = -x
 u = np.zeros((T+1, N+1))
+vi = np.zeros((T+1, N+1))
+u[:, -1] = 1.0
+print(u[0, :])
+for t in range(T+1):
+    vi[t, :] = u[t, :]-x
+print(vi[0, :])
 
 #===============================================================================
 u_FE = np.zeros((T+1, N+1))
@@ -20,7 +23,7 @@ v_FE = s.ForwardEuler(alpha, vi, T, N)
 for t in range(0, T+1):
     u_FE[t, :] = v_FE[t, :] + x
 plt.figure(1)
-for t in [0, 10, 100, 200]:
+for t in [0, 1, 10, 100, 200]:
     plt.plot(x, u_FE[t, :], label='t={}'.format(t))
     plt.title('Forward Euler')
     plt.legend()
@@ -30,7 +33,7 @@ v_BE = s.BackwardEuler(alpha, vi, T, N)
 for t in range(0, T+1):
     u_BE[t, :] = v_BE[t, :] + x
 plt.figure(2)
-for t in [0, 10, 100, 200]:
+for t in [0, 1, 10, 100, 200]:
     plt.plot(x, u_BE[t, :], label='t={}'.format(t))
     plt.title('Backward Euler')
     plt.legend()
@@ -40,15 +43,15 @@ v_CN = s.CrankNicolson(alpha, vi, T, N)
 for t in range(0, T+1):
     u_CN[t, :] = v_CN[t, :] + x
 plt.figure(3)
-for t in [0, 10, 100, 200]:
+for t in [0, 1, 10, 100, 200]:
     plt.plot(x, u_CN[t, :], label='t={}'.format(t))
     plt.title('Crank Nicolson')
     plt.legend()
 
 plt.figure(4)
-for t in [0, 10, 100, 200]:
+for t in [0, 1, 10, 100, 200]:
     plt.plot(x, u_CN[t, :], color='b', label='CrankNicolson')
-for t in [0, 10, 100, 200]:
+for t in [0, 1, 10, 100, 200]:
     plt.plot(x, u_BE[t, :], color='r', label='BackwardEuler')
 plt.plot()
 plt.show()
--- Problemsets/5th/solver.py
+++ Problemsets/5th/solver.py
@@ -5,8 +5,8 @@ import scipy.linalg as sl
 
 @numba.njit(cache = True)
 def FwdStep(alpha, vPrev, N):
-    v = np.zeros(N+1)
-    for x in range(1,N):
+    v = np.zeros(N-1)
+    for x in range(0,N-1):
         v[x] = alpha*vPrev[x-1] + (1.0-2*alpha)*vPrev[x] + alpha*vPrev[x+1]
     return v
 """
@@ -25,17 +25,17 @@ def LU_decomp(alpha, vPrev, N):
 
 def ForwardEuler(alpha, v, T, N):
     for t in range(1, T+1):
-        v[t, :] = FwdStep(alpha, v[t-1, :], N)
+        v[t, 1:-1] = FwdStep(alpha, v[t-1, :], N)
     return v
 
 def BackwardEuler(alpha, v, T, N):
     for t in range(1, T+1):
         v[t, 1:-1] = LU_decomp(alpha, v[t-1, 1:-1], N)
-    print(v[0, :])
+    print(v[0, :]) # BURDE VÆRE UENDRET [0, 0, ..., 0, 0, 1], MEN ER IKKE DET!
     return v
 
 def CrankNicolson(alpha, v, T, N):
     for t in range(1, T+1):
-        vNew = FwdStep(alpha/2, v[t-1, :], N)
-        v[t, 1:-1] = LU_decomp(alpha/2, vNew[1:-1], N)
+        v[t, 1:-1] = FwdStep(alpha/2, v[t-1, :], N)
+        v[t, 1:-1] = LU_decomp(alpha/2, v[t, 1:-1], N)
     return v
