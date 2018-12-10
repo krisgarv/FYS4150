6529cb8ff7d8c89922ddf1c9b62969639f75d2f6
krisgarv <kristinegarvin@gmail.com>
2018-12-10 13:18:15 +0100

    Skjønner ikke hva som skjer med forward euler?

--- Problemsets/5th/solver.py
+++ Problemsets/5th/solver.py
@@ -10,10 +10,39 @@ def FwdStep(alpha, vPrev, N):
         v[x] = alpha*vPrev[x-1] + (1.0-2*alpha)*vPrev[x] + alpha*vPrev[x+1]
     return v
 """
+
 def FwdStep(alpha, vPrev, N):
     A = np.zeros((N+1, N+1)) + np.diag(2.0-2.0*alpha*np.ones(N+1)) + \
     np.diag((alpha)*np.ones(N), k=1) + np.diag((alpha)*np.ones(N), k=-1)
-    v = A.dot(vPrev)
+    v = np.matmul(A, vPrev)
+    return v
+def LU_decomp(alpha, vPrev, N):
+    A = np.zeros((N+1, N+1)) + np.diag(1.0+2.0*alpha*np.ones(N+1)) + \
+    np.diag(-alpha*np.ones(N), k=1) + np.diag(-alpha*np.ones(N), k=-1)
+    lu, piv = sl.lu_factor(A, overwrite_a=True, check_finite=False)
+    LHS = sl.lu_solve((lu, piv), vPrev, trans=0, overwrite_b=True, check_finite=False)
+    return LHS
+def ForwardEuler(alpha, v, T, N):
+    for t in range(1, T+1):
+        v[t, :] = FwdStep(alpha, v[t-1, :], N)
+    return v
+
+def BackwardEuler(alpha, v, T, N):
+    for t in range(1, T+1):
+        v[t, :] = LU_decomp(alpha, v[t-1, :], N)
+    print(v[0, :]) # BURDE VÆRE UENDRET [0, 0, ..., 0, 0, 1], MEN ER IKKE DET!
+    return v
+
+def CrankNicolson(alpha, v, T, N):
+    for t in range(1, T+1):
+        v[t, :] = FwdStep(alpha/2, v[t-1, :], N)
+        v[t, :] = LU_decomp(alpha/2, v[t, :], N)
+    return v
+
+def FwdStep(alpha, vPrev, N):
+    A = np.zeros((N-1, N-1)) + np.diag(2.0-2.0*alpha*np.ones(N-1)) + \
+    np.diag((alpha)*np.ones(N-2), k=1) + np.diag((alpha)*np.ones(N-2), k=-1)
+    v = np.matmul(A, vPrev)
     return v
 """
 def LU_decomp(alpha, vPrev, N):
@@ -23,9 +52,11 @@ def LU_decomp(alpha, vPrev, N):
     LHS = sl.lu_solve((lu, piv), vPrev, trans=0, overwrite_b=True, check_finite=False)
     return LHS
 
+
 def ForwardEuler(alpha, v, T, N):
     for t in range(1, T+1):
         v[t, 1:-1] = FwdStep(alpha, v[t-1, :], N)
+    print(v[0, :])
     return v
 
 def BackwardEuler(alpha, v, T, N):
