3ce64880203907ef87c97336098d7373035219a9
Silje Christine Iversen <siljeci@student.matnat.uio.no>
2018-12-12 00:48:23 +0100

    WOHO it works

--- Problemsets/5th/run.py
+++ Problemsets/5th/run.py
@@ -7,77 +7,101 @@ sns.set_style('ticks')
 #sns.set_context('talk')
 
 #===================Initializing input===============================
-N = 100
-T = 20000
-dx=1.0/N
-dt=1.0/T
-alpha = dt/(dx*dx)
-#t_list=[0, 1, 10, 100, 200]
-t_list=[0, 10, 1000]
-# Initial conditions v(0, x) = u(0, x) - x = -x
-x = np.linspace(0, 1, N+2)
-# Initial matrix for storing v-values
-# v[t, x]
-u = np.zeros((T+1, N+2))
-vi = np.zeros((T+1, N+2))
-u[:, -1] = 1.0
-u[0, :] = 0.0
-for t in range(T+1):
-    vi[t, :] = u[t, :]-x
+N       = 100
+dx      = 1.0/N
+dt      = 0.5*(dx**2)
+alpha   = dt/(dx**2)
+
+# The time-values we want to plot. Minimum time is 0, maximum is 1:
+t_list  = [0.001, 0.01, 0.03, 0.08, 0.3, 1]
+t_max   = 1
+# Number of time steps from 0 to 1:
+T       = int(t_max/dt)
+
+# The x-range from 0 to 1 (scaled, original was from 0 to L):
+x   = np.linspace(0, 1, N+2)
+
+# The u(t,x) and w(t,x):
+u   = np.zeros((T, N+2)) #Gammel: u = np.zeros((T+1, N+2))
+vi  = np.zeros((T, N+2)) #Gammel: vi = np.zeros((T+1, N+2))
+
+# Setting the initial condition and the boundary condition for u(t,x):
+u[:, -1]   = 1.0
+u[0, :]    = 0.0
+
+# Setting the initial condition and the boundary condition for w(t,x):
+for t in range(T): #Gammel: range(T+1)
+    vi[t, :] = u[t, :] - x
+
 vi[:,0] = vi[:,N] = 0.0
 
-#===============Analytic solution================================================================
-"""
-u_FE = np.zeros_like(u)
+
+#===============Numerical solution================================================================
+
+# Forward Euler:
+u_FE        = np.zeros_like(u)
 u_FE[:, -1] = 1.0
-v_FE = s.ForwardEuler(alpha, vi, N, T)
-for t in range(0, T+1):
+v_FE        = s.ForwardEuler(alpha, vi, N, T)
+for t in range(0, T): #Gammel: T+1
     u_FE[t, 1:-1] = v_FE[t, 1:-1] + x[1:-1]
 plt.figure(1)
 for t in t_list:
-    plt.plot(x, u_FE[t, :], label='t={}'.format(t))
+    ind_t = int(t*T)-1
+    print(ind_t)
+    plt.plot(x, u_FE[ind_t, :], label='t={}'.format(t))
     plt.title('Forward Euler')
     plt.legend()
 
-u_BE = np.zeros_like(u)
+# Backward Euler:
+u_BE        = np.zeros_like(u)
 u_BE[:, -1] = 1.0
-v_BE = s.BackwardEuler(alpha, vi, N, T)
-for t in range(0, T+1):
+v_BE        = s.BackwardEuler(alpha, vi, N, T)
+for t in range(0, T): #Gammel: T+1
     u_BE[t, 1:-1] = v_BE[t, 1:-1] + x[1:-1]
 plt.figure(2)
 for t in t_list:
-    plt.plot(x, u_BE[t, :], label='t={}'.format(t))
+    ind_t = int(t*T)-1
+    plt.plot(x, u_BE[ind_t, :], label='t={}'.format(t))
     plt.title('Backward Euler')
     plt.legend()
 
-u_CN = np.zeros_like(u)
+# Crank Nicolson:
+u_CN        = np.zeros_like(u)
 u_CN[:, -1] = 1.0
-v_CN = s.CrankNicolson_LU(alpha, vi, N, T)
-for t in range(0, T+1):
+v_CN        = s.CrankNicolson_LU(alpha, vi, N, T)
+for t in range(0, T): #Gammel: T+1
     u_CN[t, 1:-1] = v_CN[t, 1:-1] + x[1:-1]
 plt.figure(3)
 for t in t_list:
-    plt.plot(x, u_CN[t, :], label='t={}'.format(t))
+    ind_t = int(t*T)-1
+    plt.plot(x, u_CN[ind_t, :], label='t={}'.format(t))
     plt.title('Crank Nicolson, LU')
     plt.legend()
 
+"""
 for i, t in enumerate(t_list):
-    plt.figure(5+i)
-    plt.plot(x, u_BE[t, :], 'C0', label='BE, t={}'.format(t))
-    plt.plot(x, u_FE[t, :], 'C1', label='FE, t={}'.format(t))
-    plt.plot(x, u_CN[t, :], 'C2', label='CN, t={}'.format(t))
+    ind_t = int(t*T)-1
+    plt.figure(4+i)
+    plt.plot(x, u_BE[ind_t, :], 'C0', label='BE, t={}'.format(t))
+    plt.plot(x, u_FE[ind_t, :], 'C1', label='FE, t={}'.format(t))
+    plt.plot(x, u_CN[ind_t, :], 'C2', label='CN, t={}'.format(t))
     plt.legend(loc='best')
 """
-plt.figure()
-for i, t in enumerate(t_list):
-    u=np.zeros_like(x)
-    v=np.zeros_like(x)
-    for n in range(N+1):
-        v += np.sin(np.pi*n*x)*np.exp(-np.pi*n**2*float(t))/float(np.pi*n)
-    u=x+2*v
-    plt.plot(u, x, 'C{}'.format(i), label='t={}'.format(t))
-    plt.xlabel('x')
-    plt.ylabel('u(t, x)')
-    plt.legend()
-    plt.title('Analytical soluion of 1D diffusion equation')
-plt.show()
+
+def analytic_1D():
+    plt.figure()
+    for i, t in enumerate(t_list):
+        u = np.zeros_like(x)
+        v = np.zeros_like(x)
+        for n in range(1, N+1):
+            v += (-1)**n*2.0*np.sin(np.pi*n*x)*np.exp(-np.pi**2*n**2*float(t))/(np.pi*n)
+
+        u = v + x
+        plt.plot(x, u, 'C{}'.format(i), label='t={}'.format(t))
+        plt.xlabel('x')
+        plt.ylabel('u(t, x)')
+        plt.legend()
+        plt.title('Analytical soluion of 1D diffusion equation')
+    plt.show()
+
+analytic_1D()
--- Problemsets/5th/solver.py
+++ Problemsets/5th/solver.py
@@ -10,6 +10,7 @@ def FwdStep(alpha, vPrev, N):
         v[x] = alpha*vPrev[x-1] + (1.0-2*alpha)*vPrev[x] + alpha*vPrev[x+1]
     return v
 """
+
 def tridiag(alpha, u, N):
     b = 2.0 + 2.0*alpha
     a = c = -alpha
@@ -27,9 +28,9 @@ def tridiag(alpha, u, N):
 
 def ForwardEuler(alpha, v, N, T):
 	#Explicit forward euler SCHEME
-	for t in range(1, T+1):
+	for t in range(1, T): #T+1
 		for x in range(1, N):
-			v[t,x]= alpha*v[t-1,x-1] + (1.0-2.0*alpha)*v[t-1,x] + alpha*v[t-1,x+1]
+			v[t,x] = alpha*v[t-1,x-1] + (1.0-2.0*alpha)*v[t-1,x] + alpha*v[t-1,x+1]
 	return v
 
 def LU_decomp(A, vPrev, N):
@@ -40,20 +41,21 @@ def LU_decomp(A, vPrev, N):
 def BackwardEuler(alpha, v, N, T):
     A = np.zeros((N, N)) + np.diag(1.0+2.0*alpha*np.ones(N)) + \
     np.diag(-alpha*np.ones(N-1), k=1) + np.diag(-alpha*np.ones(N-1), k=-1)
-    for t in range(1, T+1):
+    for t in range(1, T): #Gammel: T+1
         v[t, 1:-1] = LU_decomp(A, v[t-1, 1:-1], N)
     return v
 
 def CrankNicolson_LU(alpha, v, N, T):
     A = np.zeros((N, N)) + np.diag(2.0+2.0*alpha*np.ones(N)) + \
     np.diag(-alpha*np.ones(N-1), k=1) + np.diag(-alpha*np.ones(N-1), k=-1)
-    for t in range(1, T+1):
+    for t in range(1, T): #Gammel: T+1
         for x in range(1, N):
             v[t, x]= alpha*v[t-1,x-1] + (2.0-2.0*alpha)*v[t-1,x] + alpha*v[t-1,x+1]
             #v[t, 1:-1] = 2.0*FwdStep(alpha/2.0, v[t-1, :], N)
         v[t, 1:-1] = LU_decomp(A, v[t, 1:-1], N)
     return v
 
+#HAR IKKE FIKSET TIDEN PAA DENNE:
 def CrankNicolson_TD(alpha, v, N, T):
     for t in range(1, T+1):
         for x in range(1, N):
