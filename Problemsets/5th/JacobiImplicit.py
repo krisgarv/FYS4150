e827638380f9588800193bc163cda541baae6798
krisgarv <kristinegarvin@gmail.com>
2018-12-11 21:48:02 +0100

    Lagre mens det virker:)

--- Problemsets/5th/JacobiImplicit.py
+++ Problemsets/5th/JacobiImplicit.py
@@ -12,38 +12,48 @@ def JacobiImplicit(alpha, u, N, T):
     uPrev = u
     uTmp = uPrev
     while (diff > eps and iter < maxiter):
-        #diff = 0.0
+        diff = 0.0
         for t in range(1, T):
             for x in range(1, N+1):
                 for y in range(1, N+1):
                     u[x, y] = (1.0/float(1.+4.*alpha))*(uPrev[x,y] + \
-                    alpha*(u_temp[x+1,y] + u_temp[x-1,y] + u_temp[x,y+1] + \
-                    u_temp[x,y-1]))
+                    alpha*(uTmp[x+1,y] + uTmp[x-1,y] + uTmp[x,y+1] + \
+                    uTmp[x,y-1]))
             iter += 1
             diff /= N*2
             uTmp = uPrev
             uPrev = u
     return u
 
-Nx = 50
-Ny = 50
-dx = 1.0/Nx
+N = 50
+N = 50
+dx = 1.0/N
 dt = 0.001
 T  = 1000
 L=1.0
 Tfinal=0.1
 alpha=dt/float(dx**2)
-x = np.linspace(0, 1, Nx+2)
-y = np.linspace(0, 1, Ny+2)
+x = np.linspace(0, 1, N+2)
+y = np.linspace(0, 1, N+2)
 time = np.linspace(0, Tfinal, T)
-u = np.zeros(Nx+2, Ny+2)
-u[:, -1] = u[i, 0] = 1.0
+u = np.zeros((N+2, N+2))
+u[:, -1] = u[:, 0] = 1.0
 u[-1, :] = 1.0
 
 v = JacobiImplicit(alpha, u, N, T)
 
 X,Y =np.meshgrid(x,y)
-plt.subplots()
+plt.contour(X, Y, u)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+"""
+fig, ax = plt.figure()
+
+
+fig= plt.figure()
+plt.hold('on')
 ax = fig.add_subplot(111)
 
 
@@ -56,5 +66,5 @@ plt.ylabel('$y$')
 plt.title('2D diffusion equation with dx= %1.2f, dt= %1.3f, alpha= %1.1f, t= %1.1f '%(dx,dt,alpha,Tfinal))
 plt.legend(loc= 'best')
 #plt.savefig('boundaryhallo1.pdf')
-
+"""
 plt.show()
