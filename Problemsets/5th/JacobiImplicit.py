7a4577aefeba97a4e82ae034f2f4134027d6b878
Silje Christine Iversen <siljeci@student.matnat.uio.no>
2018-12-12 22:18:22 +0100

    ?

--- Problemsets/5th/JacobiImplicit.py
+++ Problemsets/5th/JacobiImplicit.py
@@ -3,24 +3,24 @@ import matplotlib.pyplot as plt
 import solver as s
 
 def JacobiImplicit(alpha, u, N, T):
-    eps = 10e-8
-    diff = eps + 1
+    tol = 10e-7
+    diff = tol + 1
     iter = 0
     maxiter = N**3
     uPrev = np.zeros((N+2, N+2))
     uTmp = np.zeros_like(uPrev)
     uPrev = u
     uTmp = uPrev
-    while (diff > eps and iter < maxiter):
+    while (diff >= tol and iter < maxiter):
         diff = 0.0
         for t in range(1, T+1):
             for x in range(1, N+1):
                 for y in range(1, N+1):
-                    u[x, y] = (1.0/(1.0+4.0*alpha))*(uPrev[x,y] + \
-                    alpha*(uTmp[x+1,y] + uTmp[x-1,y] + uTmp[x,y+1] + \
-                    uTmp[x,y-1]))
+                    u[y, x] = (1.0/(1.0+4.0*alpha))*(uPrev[y, x] + \
+                    alpha*(uTmp[y, x+1] + uTmp[y, x-1] + uTmp[y+1, x] + \
+                    uTmp[y-1, x]))
             iter += 1
-            diff /= N*2
+            diff = np.max(np.abs(u - uTmp))
             uTmp = uPrev
             uPrev = u
     return u
@@ -56,17 +56,31 @@ t_max   = 1
 T       = int(t_max/dt)
 
 # The x- and y-range from 0 to 1 (scaled, original was from 0 to L):
-x   = np.linspace(0, 1, N+2)
+x = np.linspace(0, 1, N+2)
 y = np.linspace(0, 1, N+2)
 
 u = np.zeros((N+2, N+2))
-u[:, -1] = u[:, 0] = 1.0 #????
-u[-1, :] = 1.0 #?????
+x_p = 0
+y_p = 0
+for x_ in x:
+    for y_ in y:
+        u[y_p, x_p] = np.sin(np.pi*x_)*np.sin(np.pi*y_) #t = 0
+        y_p += 1
+    y_p = 0
+    x_p += 1
+print(u[:,-1])
+u[:, 0] = 0.0
+u[:,-1] = 0.0
+u[0,:] = 0.0
+u[-1,:] = 0.0
+#u[:, -1] = u[:, 0] = 1.0
+#u[-1, :] = 1.0
+#implementere t = 0
 
 v = JacobiImplicit(alpha, u, N, T)
 
 X,Y =np.meshgrid(x,y)
-plt.contour(X, Y, v)
+plt.contour(Y, X, v)
 plt.title('Numerical soluion of 2D diffusion equation')
 plt.xlabel('x')
 plt.ylabel('y')
